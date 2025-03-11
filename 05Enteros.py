4f4d24e46e48a574b9f57bf787d09abf42ac79c7
Piotrek Budzowski <piobudzows@gmail.com>
2025-03-11 14:48:15 +0100

    Create 05Enteros.py

---
+++ 05Enteros.py
@@ -0,0 +1,8 @@
+# Definir una lista por asignación con 5 enteros.
+# Mostrar por pantalla solo los elementos con valor iguales o superiores a 7.
+
+lista = [12, 13, 2, 123, 11]
+
+for var in lista:
+    if var >= 7:
+        print(var)
